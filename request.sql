8aeafde7226fb6798f20072f0c99a4591fefdca4
raul.zannoni <raul.zannoni92@gmail.com>
2023-05-23 10:43:02 +0200

    Request 2

--- request.sql
+++ request.sql
@@ -9,3 +9,9 @@ ORDER BY nom_personnage
 /*Request 1*/
 SELECT nom_lieu FROM lieu
 WHERE nom_lieu LIKE '%um'
+
+/*Request 2*/
+SELECT personnage.id_lieu, lieu.nom_lieu, COUNT(nom_personnage) FROM personnage 
+INNER JOIN lieu ON personnage.id_lieu = lieu.id_lieu
+GROUP BY personnage.id_lieu
+ORDER BY COUNT(nom_personnage) DESC
